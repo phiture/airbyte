32cc2a84075b90c4db079a552063eb38dc8413ca
Stepan Chatalyan <stepan.chatalyan@phiture.com>
2022-09-07 18:14:14 +0200

    feat: add stream report service (kpis stream is disabled, but we keep the code for future integration)

--- airbyte-integrations/connectors/source-adjust/source_adjust/source.py
+++ airbyte-integrations/connectors/source-adjust/source_adjust/source.py
@@ -1,125 +1,18 @@
 #
 # Copyright (c) 2022 Airbyte, Inc., all rights reserved.
 #
-
-
-from abc import ABC
 from datetime import date, datetime
-from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
+from typing import Any, List, Mapping, Tuple
 
-import requests
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
-from airbyte_cdk.sources.streams.http import HttpStream
 from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
-
-"""
-TODO: Most comments in this class are instructive and should be deleted after the source is implemented.
-
-This file provides a stubbed example of how to use the Airbyte CDK to develop both a source connector which supports full refresh or and an
-incremental syncs from an HTTP API.
-
-The various TODOs are both implementation hints and steps - fulfilling all the TODOs should be sufficient to implement one basic and one incremental
-stream from a source. This pattern is the same one used by Airbyte internally to implement connectors.
-
-The approach here is not authoritative, and devs are free to use their own judgement.
-
-There are additional required TODOs in the files within the integration_tests folder and the spec.yaml file.
-"""
+from source_adjust.streams_report_service import ReportService
 
 
-# Basic full refresh stream
-class AdjustStream(HttpStream, ABC):
-    url_base = "https://api.adjust.com"
-
-    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
-        """
-        TODO: Override this method to define a pagination strategy. If you will not be using pagination, no action is required - just return None.
-
-        This method should return a Mapping (e.g: dict) containing whatever information required to make paginated requests. This dict is passed
-        to most other methods in this class to help you form headers, request bodies, query params, etc..
-
-        For example, if the API accepts a 'page' parameter to determine which page of the result to return, and a response from the API contains a
-        'page' number, then this method should probably return a dict {'page': response.json()['page'] + 1} to increment the page count by 1.
-        The request_params method should then read the input next_page_token and set the 'page' param to next_page_token['page'].
-
-        :param response: the most recent response from the API
-        :return If there is another page in the result, a mapping (e.g: dict) containing information needed to query the next page in the response.
-                If there are no more pages in the result, return None.
-        """
-        return None
-
-    def request_params(
-            self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
-    ) -> MutableMapping[str, Any]:
-        """
-        TODO: Override this method to define any query parameters to be set. Remove this method if you don't need to define request params.
-        Usually contains common params e.g. pagination size etc.
-        """
-        return {}
-
-    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
-        """
-        TODO: Override this method to define how a response is parsed.
-        :return an iterable containing each record in the response
-        """
-        yield {}
-
-
-class Kpis(AdjustStream):
-    # TODO: Fill in the primary key.
-    #  This is usually a unique field in the stream, like an ID or a timestamp.
-    #  Required.
-    primary_key = "app_token"
-
-    def __init__(self, config: Mapping[str, Any], **kwargs):
-        super().__init__(**kwargs)
-        self.app_token = config['app_token']
-
-    def path(
-            self,
-            stream_state: Mapping[str, Any] = None,
-            stream_slice: Mapping[str, Any] = None,
-            next_page_token: Mapping[str, Any] = None
-    ) -> str:
-        """
-        TODO: Override this method to define the path this stream corresponds to.
-         E.g. if the url is https://example-api.com/v1/customers then this should return "customers".
-         Required.
-        """
-        return f"kpis/v1/{self.app_token}.json"
-
-    def request_params(
-            self,
-            stream_state: Mapping[str, Any],
-            stream_slice: Mapping[str, Any] = None,
-            next_page_token: Mapping[str, Any] = None,
-    ) -> MutableMapping[str, Any]:
-        return {}
-
-    def parse_response(
-            self,
-            response: requests.Response,
-            stream_state: Mapping[str, Any],
-            stream_slice: Mapping[str, Any] = None,
-            next_page_token: Mapping[str, Any] = None,
-    ) -> Iterable[Mapping]:
-        # The response is a simple JSON whose schema matches our stream's schema exactly,
-        # so we just return a list containing the response
-        return [response.json()]
-
-    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
-        # The API does not offer pagination,
-        # so we return None to indicate there are no more pages in the response
-        return None
-
-
-# Source
 class SourceAdjust(AbstractSource):
     def check_connection(self, logger, config) -> Tuple[bool, any]:
         """
-        TODO: Implement a connection check to validate that the user-provided config can be used to connect to the underlying API
-
         See https://github.com/airbytehq/airbyte/blob/master/airbyte-integrations/connectors/source-stripe/source_stripe/source.py#L232
         for an example.
 
@@ -145,10 +38,13 @@ class SourceAdjust(AbstractSource):
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
         """
-        TODO: Replace the streams below with your own streams.
+        Define your streams here.
 
         :param config: A Mapping of the user input configuration as defined in the connector spec.
         """
-        # TODO remove the authenticator if not required.
-        auth = TokenAuthenticator(token=f"token={config['user_token']}", auth_method="Token")
-        return [Kpis(authenticator=auth, config=config)]
+        # auth = TokenAuthenticator(token=f"token={config['user_token']}", auth_method="Token")
+        auth_report_service = TokenAuthenticator(token=config['user_token'])
+        return [
+            # Kpis(authenticator=auth, config=config)
+            ReportService(authenticator=auth_report_service, config=config)
+        ]
---
+++ airbyte-integrations/connectors/source-adjust/source_adjust/streams_report_service.py
@@ -0,0 +1,110 @@
+#
+# Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+#
+from typing import Any, Iterable, Mapping, MutableMapping, Optional, List
+
+import pendulum
+import requests
+from airbyte_cdk.models import SyncMode
+
+from source_adjust.streams import AdjustStream
+from source_adjust.util_report_service import dimensions, metrics
+
+
+class ReportService(AdjustStream):
+    url_base = "https://dash.adjust.com"
+
+    primary_key = dimensions
+    cursor_field = "day"
+    time_interval = {"days": 1}
+    state_checkpoint_interval = 1000
+
+    def __init__(self, config: Mapping[str, Any], **kwargs):
+        super().__init__(**kwargs)
+        self._app_token = config['app_token']
+        self._start_date = config['start_date']
+        self._end_date = config.get('end_date')
+        self._state = {}
+
+    @property
+    def state(self) -> MutableMapping[str, Any]:
+        return self._state
+
+    @state.setter
+    def state(self, value: MutableMapping[str, Any]):
+        self._state.update(value)
+
+    def current_state(self, canvas_id, default=None):
+        default = default or self.state.get(self.cursor_field)
+        return self.state.get(canvas_id, {}).get(self.cursor_field) or default
+
+    def path(
+            self,
+            stream_state: Mapping[str, Any] = None,
+            stream_slice: Mapping[str, Any] = None,
+            next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        return "control-center/reports-service/report"
+
+    def request_params(
+            self,
+            stream_state: Mapping[str, Any],
+            stream_slice: Mapping[str, Any] = None,
+            next_page_token: Mapping[str, Any] = None,
+    ) -> MutableMapping[str, Any]:
+        return {
+            "app_token__in": self._app_token,
+            "date_period": f'{stream_slice["start_date"]}:{stream_slice["end_date"]}',
+            "dimensions": ",".join(dimensions),
+            "metrics": ",".join(metrics),
+        }
+
+    def parse_response(
+            self,
+            response: requests.Response,
+            stream_state: Mapping[str, Any],
+            stream_slice: Mapping[str, Any] = None,
+            next_page_token: Mapping[str, Any] = None,
+    ) -> Iterable[Mapping]:
+        data = response.json()
+        for row in data['rows']:
+            yield row
+
+    def stream_slices(self, sync_mode: SyncMode, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, Any]]]:
+        stream_state = stream_state or {}
+
+        if stream_state.get('last_sync'):
+            start_date = pendulum.parse(stream_state['last_sync'].get(self.cursor_field))
+        else:
+            start_date = pendulum.parse(self._start_date)
+
+        end_date = pendulum.parse(self._end_date or pendulum.now().to_date_string())
+
+        while start_date <= end_date and start_date < pendulum.parse(pendulum.now().to_date_string()):
+            starting_at = start_date
+            ending_at = start_date
+
+            self.logger.info(
+                f"Fetching {self.name} ; time range: {starting_at.to_date_string()} - {ending_at.to_date_string()}"
+            )
+
+            yield {
+                "start_date": starting_at.to_date_string(),
+                "end_date": ending_at.to_date_string(),
+            }
+            start_date = start_date.add(**self.time_interval)
+
+    def read_records(
+            self, sync_mode: SyncMode, cursor_field: List[str] = None, stream_slice: MutableMapping[str, Any] = None, **kwargs
+    ) -> Iterable[Mapping[str, Any]]:
+        for record in super().read_records(sync_mode=sync_mode, stream_slice=stream_slice):
+            current_state = self.current_state(self._app_token)
+            if current_state:
+                date_in_current_stream = pendulum.parse(current_state)
+                date_in_latest_record = pendulum.parse(record[self.cursor_field])
+                cursor_value = (max(date_in_current_stream, date_in_latest_record)).to_date_string()
+                self.state = {self._app_token: {self.cursor_field: cursor_value}}
+                yield record
+                continue
+            self.state = {self._app_token: {self.cursor_field: record[self.cursor_field]}}
+            yield record
